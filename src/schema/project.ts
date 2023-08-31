2da2f40f494c9a74391ea3f24e475a3a8b0c370c
nelsonhp3 <engnelsonhenrique@gmail.com>
2023-08-30 22:52:44 -0300

    Adds project object and Writer now creates bcf.version, project.bcfp and extensions.xsd

---
+++ src/schema/project.ts
@@ -0,0 +1,29 @@
+import { IMarkup } from "./markup"
+
+export interface IProject {
+    project_id: string
+    name: string
+    version: string
+    markups?: IMarkup[]
+    extension_schema: IExtensionsSchema | undefined
+}
+
+export interface IExtensionsSchema {
+    schema: {
+        xmlns: string
+        redefine: {
+            schema_location: string
+            simple_type: [
+                name: string,
+                restriction: {
+                    base: string
+                    enumeration: [
+                        {
+                            value: string
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+}
